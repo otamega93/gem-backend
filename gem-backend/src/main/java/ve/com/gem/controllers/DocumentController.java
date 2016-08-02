b9f4d118a4198f5fdd52ff7d7287f2c610f03aa8
informatica <gomezcarlose@gmail.com>
2016-08-02 19:02:14 -0400

    ADD imports

--- gem-backend/src/main/java/ve/com/gem/controllers/DocumentController.java
+++ gem-backend/src/main/java/ve/com/gem/controllers/DocumentController.java
@@ -1,5 +1,19 @@
 package ve.com.gem.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 public class DocumentController {
 
 	public DocumentController() {
--- gem-backend/src/main/java/ve/com/gem/controllers/GemController.java
+++ gem-backend/src/main/java/ve/com/gem/controllers/GemController.java
@@ -7,7 +7,6 @@ import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
