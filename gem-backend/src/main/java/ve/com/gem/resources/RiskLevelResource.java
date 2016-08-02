eb51cb0119fe0339b8542244c007d1d190aa5dac
mradrox <alejandrojmorenor@gmail.com>
2016-08-02 15:23:45 -0400

    Create Resource

---
+++ gem-backend/src/main/java/ve/com/gem/resources/RiskLevelResource.java
@@ -0,0 +1,62 @@
+package ve.com.gem.resources;
+
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.core.Relation;
+
+@Relation(collectionRelation = "riskLevels")
+public class RiskLevelResource extends ResourceSupport {
+
+	private Long ids;
+	private String name;
+	private String description;
+
+	public RiskLevelResource() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the ids
+	 */
+	public Long getIds() {
+		return ids;
+	}
+
+	/**
+	 * @param ids
+	 *            the ids to set
+	 */
+	public void setIds(Long ids) {
+		this.ids = ids;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description
+	 *            the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
